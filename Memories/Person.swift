489a62bfcc2baed25281c6523d742c2e38c4ea94
Taha alyafai <58830733+tahapp@users.noreply.github.com>
2023-12-11 16:24:42 -0500

    Add files via upload

---
+++ Memories/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  10 name to faces
+//
+//  Created by Taha Saleh on 7/29/22.
+//
+
+import Foundation
+
+class Person
+{
+    var name:String
+    var image:String
+    
+    init(name:String,image:String)
+    {
+        self.name = name
+        self.image = image
+    }
+}
